c7a2cdb920bcf884d87f5d0d88c32b331fcdc9cd
Jay Kumar <jr-094@hotmail.com>
2017-01-28 14:57:05 -0500

    Merged Kevin and Jay Code (Partial)

--- mobile/src/main/java/com/example/jeremy/androidwearheartrate/DetailedWarningActivity.java
+++ mobile/src/main/java/com/example/jeremy/androidwearheartrate/DetailedWarningActivity.java
@@ -1,14 +1,15 @@
 package com.example.jeremy.androidwearheartrate;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 
-import com.github.lzyzsd.circleprogress.ArcProgress;
+import com.example.jeremy.androidwearheartrate.database.DatabaseChangeListener;
+import com.example.jeremy.androidwearheartrate.database.FirebaseDB;
 import com.example.jeremy.androidwearheartrate.warnings.Warning;
 import com.example.jeremy.androidwearheartrate.warnings.WarningIdKeys;
+import com.github.lzyzsd.circleprogress.ArcProgress;
 
 public class DetailedWarningActivity extends FragmentActivity {
 
@@ -18,23 +19,34 @@ public class DetailedWarningActivity extends FragmentActivity {
         setContentView(R.layout.activity_detailed_warning);
 
         Intent intent = getIntent();
-        String id = intent.getStringExtra(MainActivity.WARNING_ID);
-        TextView textView = (TextView) findViewById(R.id.warning_details);
-        ArcProgress myArc = (ArcProgress) findViewById(R.id.heart_rate);
+        final String id = intent.getStringExtra(MainActivity.WARNING_ID);
+        final TextView textView = (TextView) findViewById(R.id.warning_details);
+        final ArcProgress myArc = (ArcProgress) findViewById(R.id.heart_rate);
 
         Warning globalWarning = new Warning(WarningIdKeys.NEUTRAL);
 
-        int heartRate;
-        String message;
-        if(id.contains("High")) {
-            heartRate = 120;
-            message = "High heart rate detected, have you been running? If not, don't drive";
-        } else {
-            heartRate = 30;
-            message = "Low heart rate detected, showing abnormal symptoms";
-        }
+        FirebaseDB instance = new FirebaseDB();
+        instance.onChange(new DatabaseChangeListener() {
+            @Override
+            public void onSuccess(String value) {
+                int heartRate;
+                String message;
+                if(id.contains("High")) {
+                    heartRate = 120;
+                    message = "High heart rate detected, have you been running? If not, don't drive";
+                } else {
+                    heartRate = 30;
+                    message = "Low heart rate detected, showing abnormal symptoms";
+                }
 
-        textView.setText(message);
-        myArc.setProgress(heartRate);
+                textView.setText(message);
+                myArc.setProgress(heartRate);
+            }
+
+            @Override
+            public void onFail(String value) {
+
+            }
+        });
     }
 }
